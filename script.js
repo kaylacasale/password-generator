bf5b06b24764e52cf1ac209591106ec598d176d1
Kayla Casale <kayla.casale@gmail.com>
2022-11-25 20:55:50 -0800

    cloned git link into 03-homework directory

---
+++ script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// entered my comments below
+function generatePassword() {
+
+}
+// entered my comments below
+return finishedPassword
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
